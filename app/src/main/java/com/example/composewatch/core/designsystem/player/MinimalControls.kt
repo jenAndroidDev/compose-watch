597b828b9fc82bdc94614cc02adc5f5536b283e7
Jenin Joseph <104613017+jenAndroidDev@users.noreply.github.com>
2025-04-21 14:42:48 +0530

    Ui Controls Aligned To Center.✅

--- app/src/main/java/com/example/composewatch/core/designsystem/player/MinimalControls.kt
+++ app/src/main/java/com/example/composewatch/core/designsystem/player/MinimalControls.kt
@@ -1,9 +1,13 @@
 package com.example.composewatch.core.designsystem.player
 
 import android.graphics.Color
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -16,18 +20,19 @@ import androidx.compose.ui.unit.dp
 import androidx.media3.common.Player
 
 @Composable
-fun MinimalControls(player: Player,modifier: Modifier = Modifier){
+fun MinimalControls(player: Player,modifier: Modifier){
     val graySemiTransparentBackground = androidx.compose.ui.graphics.Color.Gray.copy(alpha = 0.5f)
-    val modifierForIconButton =
-        modifier.size(80.dp).background(graySemiTransparentBackground, shape = CircleShape)
-    Row(
-        modifier = modifier.fillMaxWidth()
-        ,
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.SpaceEvenly
-    ) {
-        PreviousButton(player, modifierForIconButton)
-        PlayPauseButton(player, modifierForIconButton)
-        NextButton(player, modifierForIconButton)
-    }
-}
+
+    val modifierForIconButton = modifier.size(60.dp).background(graySemiTransparentBackground, shape = CircleShape)
+
+
+          Row(
+              modifier = modifier.fillMaxWidth(),
+              verticalAlignment = Alignment.Bottom,
+              horizontalArrangement = Arrangement.SpaceEvenly
+          ) {
+              PreviousButton(player, modifierForIconButton)
+              PlayPauseButton(player, modifierForIconButton)
+              NextButton(player, modifierForIconButton)
+          }
+      }
--- app/src/main/java/com/example/composewatch/feature/feed/presentation/FeedScreen.kt
+++ app/src/main/java/com/example/composewatch/feature/feed/presentation/FeedScreen.kt
@@ -2,15 +2,35 @@ package com.example.composewatch.feature.feed.presentation
 
 
 import android.os.Build
+import android.os.Looper
+import android.view.Surface
+import android.view.SurfaceHolder
+import android.view.SurfaceView
+import android.view.TextureView
 import androidx.annotation.OptIn
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.shrinkHorizontally
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
+import androidx.compose.foundation.Indication
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.PressInteraction
+import androidx.compose.foundation.interaction.collectIsPressedAsState
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.material3.ripple
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
@@ -23,7 +43,19 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.compose.LifecycleResumeEffect
 import androidx.lifecycle.compose.LifecycleStartEffect
+import androidx.media3.common.AudioAttributes
+import androidx.media3.common.DeviceInfo
+import androidx.media3.common.MediaItem
+import androidx.media3.common.MediaMetadata
+import androidx.media3.common.PlaybackException
+import androidx.media3.common.PlaybackParameters
 import androidx.media3.common.Player
+import androidx.media3.common.Timeline
+import androidx.media3.common.TrackSelectionParameters
+import androidx.media3.common.Tracks
+import androidx.media3.common.VideoSize
+import androidx.media3.common.text.CueGroup
+import androidx.media3.common.util.Size
 import androidx.media3.common.util.UnstableApi
 import androidx.media3.ui.compose.PlayerSurface
 import androidx.media3.ui.compose.SURFACE_TYPE_SURFACE_VIEW
@@ -31,14 +63,13 @@ import androidx.media3.ui.compose.modifiers.resizeWithContentScale
 import androidx.media3.ui.compose.state.rememberPresentationState
 import com.example.composewatch.core.designsystem.player.CONTENT_SCALES
 import com.example.composewatch.core.designsystem.player.ComposeWatchSeekBar
-import com.example.composewatch.core.designsystem.player.ExtraControls
 import com.example.composewatch.core.designsystem.player.MinimalControls
-import com.example.composewatch.core.designsystem.player.noRippleClickable
 import com.example.composewatch.core.player.ComposeWatchPlayerFactory
 import com.example.composewatch.core.player.utils.MediaSource
+import kotlinx.coroutines.delay
 
 private val url = "https://storage.googleapis.com/exoplayer-test-media-0/shortform_2.mp4"
-private val hlsUrl ="https://devstreaming-cdn.apple.com/videos/streaming/examples/adv_dv_atmos/main.m3u8"
+private const val hlsUrl ="https://devstreaming-cdn.apple.com/videos/streaming/examples/adv_dv_atmos/main.m3u8"
 
 /*1.Fix the Control Ui
 * 2.Add Seek Bar
@@ -77,7 +108,8 @@ fun FeedScreen(modifier: Modifier){
 
     player?.let {
         MediaPlayer(player = it,
-            modifier = modifier.fillMaxWidth()
+            modifier = modifier
+                .fillMaxWidth()
                 .fillMaxHeight(0.3f))
     }
 }
@@ -88,35 +120,79 @@ fun MediaPlayer(
     modifier: Modifier,
     player: Player){
 
-    val showControls by remember { mutableStateOf(true) }
+    var showControls by remember { mutableStateOf(true) }
     val currentContentScaleIndex by remember { mutableIntStateOf(0) }
     val contentScale = CONTENT_SCALES[currentContentScaleIndex].second
 
     val presentationState = rememberPresentationState(player = player)
     val scaleModifier = Modifier.resizeWithContentScale(contentScale,presentationState.videoSizeDp)
+    val interactionSource = remember { MutableInteractionSource() }
+
+
+    LaunchedEffect(interactionSource) {
+        interactionSource.interactions.collect{interaction->
+            when(interaction){
+                is PressInteraction.Press->{
+                    delay(200)
+                    showControls = false
+                }
+                is PressInteraction.Release->{
+                    delay(200)
+                    showControls = true
+                }
+            }
+        }
+    }
 
     Box(
-        modifier.aspectRatio(16f/9f),
-        contentAlignment = Alignment.TopCenter) {
+        modifier
+            .aspectRatio(16f / 9f)
+            .clickable(
+                onClick = {},
+                interactionSource = interactionSource,
+                indication = ripple()
+            ),
+        ) {
 
         PlayerSurface(
             player = player,
             surfaceType = SURFACE_TYPE_SURFACE_VIEW,
-            modifier = scaleModifier.noRippleClickable { showControls!=showControls },
+            modifier = scaleModifier,
+            )
+        val playerListener = object:Player.Listener{
+            override fun onPlaybackStateChanged(playbackState: Int) {
+                when(playbackState){
+                }
 
-        )
-        if (presentationState.coverSurface){
-            Box(modifier = modifier.matchParentSize().background(Color.Black))
+            }
         }
-        if (showControls){
-            MinimalControls(player, Modifier.align(Alignment.Center))
-//            ExtraControls(
-//                player,
-//                Modifier.fillMaxWidth()
-//                    .align(Alignment.BottomCenter)
-//                    .background(Color.Gray.copy(alpha = 0.4f))
-//                    .navigationBarsPadding(),
-//            )
+        if (presentationState.coverSurface){
+            Box(modifier = modifier
+                .matchParentSize()
+                .background(Color.Black))
+        }
+        Box(
+            modifier = Modifier
+                .fillMaxSize(), // fills the screen
+            contentAlignment = Alignment.Center // centers the content inside
+        ) {
+            AnimatedVisibility(
+                visible = showControls,
+                enter = slideInVertically(
+                    initialOffsetY = { it / 2 }
+                ),
+                exit = slideOutVertically(
+                    targetOffsetY = { it / 2 }
+                )
+            ) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    MinimalControls(player, Modifier)
+                }
+            }
         }
     }
     ComposeWatchSeekBar(player = player, modifier = modifier)
